a67eeda1e1ac795ec5a81178278cfd3369fb6df8
20120433 <82367666+hoduybao@users.noreply.github.com>
2023-12-03 15:33:04 +0700

    .

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const express=require("express")
-require('dotenv').config()
-const dbConnect=require('./config/dbConnect')
-const cookieParser=require('cookie-parser')
-const initRoutes=require('./routes')
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
+const cookieParser = require("cookie-parser");
+const initRoutes = require("./routes");
 
-const cors=require('cors');
+const cors = require("cors");
 
-const app=express();
-const port=process.env.PORT || 8888
+const app = express();
+const port = process.env.PORT || 8888;
 
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, process.env.CLIENT_URL_LOCALHOST],
+    methods: ["POST", "PUT", "GET", "DELETE"],
+  })
+);
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: ['POST','PUT','GET','DELETE'] 
-}))
-
-app.use(cookieParser())
+app.use(cookieParser());
 
 //doc data kieu json
 app.use(express.json());
 //doc data kieu array , object,...
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 dbConnect();
 
 initRoutes(app);
 
-app.listen(port,()=>{
-    console.log("Server running")
-})
+app.listen(port, () => {
+  console.log("Server running");
+});
